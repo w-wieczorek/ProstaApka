c1c57671d8b685ce39e4a36f938d2cd22547c1da
Wojciech Wieczorek <wwieczorek@ath.bielsko.pl>
2024-03-08 22:00:14 +0100

    Initial commit

---
+++ ProstaApka/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace ProstaApka.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ ProstaApka/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ProstaApka.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
